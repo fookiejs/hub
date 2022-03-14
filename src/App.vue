2da5651b1f5260bb44f4c3469436e61a2b9f5a37
umudikk <seyitumutbicer@gmail.com>
2022-03-14 15:04:29 +0300

    dev

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,49 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-const a = 1;
+import { Icon } from "@iconify/vue";
 </script>
 
 <template>
-  <div>
-    {{ a }}
+  <div
+    class="text-gray-700 w-screen h-screen border justify-center items-center"
+  >
+    <header>
+      <div class="text-6xl">Fookie JS</div>
+      <div class="text-2xl">Model based reactive javascript framework.</div>
+      <div>
+        <a href="https://github.com/fookiejs/core">
+          <button class="flex justify-center items-center">
+            <Icon icon="akar-icons:github-fill" height="32"> </Icon>
+            <div class="w-1"></div>
+            <span>Github</span>
+          </button>
+        </a>
+        <a href="https://github.com/fookiejs/core">
+          <button class="flex justify-center items-center">
+            <Icon icon="cib:postman" height="32"> </Icon>
+            <div class="w-1"></div>
+            <span>Postman</span>
+          </button>
+        </a>
+        <a href="https://github.com/fookiejs/core">
+          <button class="flex justify-center items-center">
+            <Icon icon="simple-icons:githubpages" height="32"> </Icon>
+            <div class="w-1"></div>
+            <span>Github Pages</span>
+          </button>
+        </a>
+      </div>
+    </header>
+    <main>
+      <div class="text-4xl">Utils</div>
+      <div class="">
+        <a class="text-2xl" href="https://ui.fookiejs.com">
+          <div>UI</div>
+        </a>
+        <a class="text-2xl" href="https://doc.fookiejs.com">
+          <div>DOC</div>
+        </a>
+      </div>
+    </main>
   </div>
 </template>
 
