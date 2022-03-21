ba690298d8b733800c2f0439ec94255fcdb56acf
umudik <seyitumutbicer@gmail.com>
2022-03-21 05:06:27 +0300

    dev

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template lang="pug">
 v-app(app)
+  v-app-bar(app)
+    v-container
+      v-row(align="center")
+        v-toolbar-title Fookie JS
+        v-spacer
+        v-btn(text) Doc
+        v-btn(icon)
+          v-icon mdi-discord
   v-main(app)
     v-container(app)
       router-view
 </template>
 
 <script>
-
-
 export default {
   name: "App",
-  components: {  },
-  mounted() {
-    this.$vuetify.mode="dark"
-  },
+  components: {},
+  mounted() {},
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,66 @@
 <template lang="pug">
 div
   v-row
-    v-col Fookie JS
+    v-col
+      .tw-text-8xl The Revolutionary
+      .tw-text-8xl JavaScript Framework
   v-row
-    v-col Desc
+    v-col
+      h3 Plugins
+      v-divider
+  v-row(justify="space-center")
+    v-card(v-for="plugin in plugins", width="350")
+      v-card-title {{ plugin.name }}
+      v-card-text {{ plugin.desc }}
+      v-card-text
+        v-chip(small, dark) v{{ plugin.version }}
   v-row
-    v-col Plugins
-  v-row
-    v-col(v-for="i in 6")
-      v-card()
-        v-card-title UI
+    v-col
   v-row 
-    v-col Utils
-  v-row
-    v-col(v-for="i in 2")
-      v-card()
-        v-card-title UI
+    v-col
+      h3 Utils
+      v-divider
+  v-row(justify="space-center")
+    v-card(v-for="util in utils", width="350")
+      v-card-title {{ util.name }}
+      v-card-text {{ util.desc }}
+      v-card-text
+        v-chip(small, dark) v{{ util.version }}
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      
-    }
-  }
+      plugins: [
+        {
+          name: "Server",
+          link: "https://github.com/fookiejs/cache",
+          desc: "Boktan açıklama burada.",
+          version: "1.5.4",
+        },
+        {
+          name: "Cache",
+          link: "https://github.com/fookiejs/cache",
+          desc: "Boktan açıklama burada.",
+          version: "1.5.4",
+        },
+        {
+          name: "Balancer",
+          link: "https://github.com/fookiejs/cache",
+          desc: "Boktan açıklama burada.",
+          version: "1.5.4",
+        },
+      ],
+      utils: [
+        {
+          name: "FOOKIE UI",
+          link: "google.com",
+          desc: "12312312",
+          version: "1.5.4",
+        },
+      ],
+    };
+  },
 };
 </script>
