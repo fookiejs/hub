91f06140552ca0e6c002c6af8679799a58e044e6
umudik <seyitumutbicer@gmail.com>
2022-04-03 04:16:30 +0300

    dev

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@ div(class="tw-grid tw-gap-8")
        span JavaScript Framework
   v-row
     v-col(class="tw-text-xl tw-text-center tw-text-gray-600")
-      span Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nisl est, consequat sit amet ultrices quis, congue non libero. 
+      span An application development method based on a model and its flow.
   v-row
     v-col(class="tw-flex items-center justify-center")
       v-btn(elevation="0" class="tw-mx-1" href="https://github.com/fookiejs/core" target="#") Github 
@@ -17,17 +17,19 @@ div(class="tw-grid tw-gap-8")
         v-icon(right) mdi-book-outline
       v-btn(elevation="0" class="tw-mx-1" href="https://github.com/fookiejs/examples" target="#") Examples  
         v-icon(right) mdi-file-code-outline
-
-  v-row(class="tw-text-2xl tw-text-center")
+  v-row(class=" tw-text-center")
     v-col
-      h3 Offical Plugins
-  v-row(justify="tw-grid tw-gap-4 items-center justify-center")
+      span(class="tw-text-2xl") Tool & Plugins
+  v-row(justify="tw-grid tw-grid-rows-4 tw-gap-4 justify-center")
     div(v-for="plugin in plugins")
-      v-card(width="350")
+      v-card(width="380")
         v-card-title {{ plugin.name }}
-        v-card-text {{ plugin.desc }}
+        v-card-subtitle {{ plugin.desc }}
         v-card-text
-          v-chip(small, dark) {{ plugin.version }}
+          v-chip(class="tw-font-italic" label)
+            span(class="tw-text-green-600 tw-mr-2 tw-font-bold") {{">"}}
+            span(class="tw-text-gray-900 tw-font-bold") {{plugin.npm}}
+            
   v-row
     v-col
 </template>
@@ -38,28 +40,34 @@ export default {
     return {
       plugins: [
         {
+          name: "Core",
+          link: "https://github.com/fookiejs/cache",
+          desc: "Fookie JS Core",
+          npm: "npm install fookie",
+        },
+        {
           name: "Server",
           link: "https://github.com/fookiejs/cache",
-          desc: "Boktan açıklama burada.",
-          version: "v1.5.4",
+          desc: "Allows the application to respond to http request.",
+          npm: "npm install fookie-server",
         },
         {
           name: "Databases",
           link: "https://github.com/fookiejs/cache",
-          desc: "Boktan açıklama burada.",
-          version: "v1.5.4",
+          desc: "Allows to use application with various databases.",
+          npm: "npm install fookie-databases",
         },
         {
           name: "Cache",
           link: "https://github.com/fookiejs/cache",
-          desc: "Boktan açıklama burada.",
-          version: "v1.5.4",
+          desc: "Solve cache needs in seconds.",
+          npm: "npm install fookie-cache",
         },
         {
-          name: "Balancer",
+          name: "Cron",
           link: "https://github.com/fookiejs/cache",
-          desc: "Boktan açıklama burada.",
-          version: "In Progress",
+          desc: "CronJob solution.",
+          npm: "npm install fookie-cron",
         },
       ],
       utils: [
@@ -80,5 +88,8 @@ export default {
   background: -webkit-linear-gradient(red, darkred);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
+}
+code {
+  font-family: monospace;
 }
 </style>
