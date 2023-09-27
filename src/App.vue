a7737c4a390acdb0718f2989508e981b5948cc48
umudik <seyitumutbicer@gmail.com>
2023-09-27 14:16:21 +0300

    dev

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <template lang="pug">
 v-app(app)
-  v-app-bar(app elevate-on-scroll)
+  v-app-bar(app, elevate-on-scroll)
     v-container
       v-row(align="center")
-          v-app-bar-nav-icon 
-            v-img(src="./assets/logo.png" max-height="32" contain)
-          v-app-bar-title() Fookie.js
-          v-spacer
-          v-btn(text href="mailto:umut@fookiejs.com" target="#" )  Contact
-            v-icon(right) mdi-email-outline
-          v-btn(text href="https://discord.com/invite/Y4BPxTUMsh" target="#" ) Discord
-            v-icon(right) mdi-account-group
-          v-btn(text @click="dark = !dark") {{dark ? 'Light': 'Dark'}}
-            v-icon(right) mdi-theme-light-dark
+        v-app-bar-nav-icon
+          v-img(src="./assets/logo.png", max-height="32", contain)
+        v-app-bar-title Fookie.js
+        v-spacer
+        v-btn(text, href="mailto:umut@fookiejs.com", target="#") Contact
+          v-icon(right) mdi-email-outline
+        v-btn(text, href="https://discord.com/invite/Y4BPxTUMsh", target="#") Discord
+          v-icon(right) mdi-account-group
+        v-btn(text, @click="dark = !dark") {{ dark ? "Light" : "Dark" }}
+          v-icon(right) mdi-theme-light-dark
   v-main(app)
     v-container(app)
       router-view
   v-footer(padless)
-    v-card(flat tile width="100%" class="tw-text-center")
+    v-card.tw-text-center(flat, tile, width="100%")
       v-card-text
-        | {{ new Date().getFullYear() }} &mdash; 
+        | {{ new Date().getFullYear() }} &mdash;
         strong Fookie JS
-
-        
-
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,12 @@ div(class="tw-grid tw-gap-8")
        span() The 
        span(class="grad") Revolutionary
        br
-       span JavaScript Framework
+       span TypeScript Framework
   v-row
-    v-col(class="tw-text-xl tw-text-center tw-text-gray-600")
-      span An application development method based on a model and its flow.
+    v-col(class="tw-text-xl tw-text-center")
+      span Design Once, Extend Infinitly
+      br
+      span Comprehensive model-based application development framework
   v-row
     v-col(class="tw-flex items-center justify-center")
       v-btn(elevation="0" class="tw-mx-1" href="https://github.com/fookiejs/core" target="#") Github 
@@ -20,23 +22,26 @@ div(class="tw-grid tw-gap-8")
   v-row(class="tw-text-center")
     v-col
       span(class="tw-text-2xl") Ecosystem
-  v-row(class="tw-gap-4 justify-center")
+  v-row(class="tw-grid tw-grid-cols-1 md:tw-grid-cols-3 lg:tw-grid-cols-4 tw-gap-4")
     div(v-for="repo in repos")
-      v-card(width="380" outlined)
-        v-card-title {{repo.name}}
-          v-spacer
-          v-btn(:href="repo.html_url" target="#" icon)
-            v-icon() mdi-link  
-        v-card-subtitle {{repo.description || "-"}}
-        v-card-text
-          v-chip(label outlined class="tw-mr-1") 
-            v-icon(left small) mdi-star     
-            span {{repo.stargazers_count}} 
-          v-chip(label outlined) 
-            v-icon(left small) mdi-eye     
-            span {{repo.watchers}}   
-        v-card-actions
+      v-card(outlined class="tw-w-full tw-flex tw-flex-col tw-justify-between tw-rounded-lg")
+        div
+          v-card-title(class="tw-text-xl tw-font-semibold")
+            span {{repo.name}}
+            v-spacer
+            v-btn(:href="repo.html_url" target="#" icon)
+              v-icon() mdi-link  
+          v-card-subtitle(class="tw-text-sm") {{repo.description || "-"}}
+          v-card-text
+            v-chip(label outlined class="tw-mr-1") 
+              v-icon(left small) mdi-star     
+              span {{repo.stargazers_count}} 
+            v-chip(label outlined) 
+              v-icon(left small) mdi-eye     
+              span {{repo.watchers}}
+        v-card-actions(class="tw-p-4")
           v-chip(class="tw-mr-1" v-for="t in repo.topics" small dark) {{t}}
+
 
   v-row
     v-col
